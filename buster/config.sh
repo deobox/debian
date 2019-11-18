be203ffe9967b16534b3c3d3ae890d5f12cdbc1c
Stefan Saraev <stefan@saraev.ca>
2019-11-18 14:17:58 +0200

    remove net.ifnames=0

--- buster/config.sh
+++ buster/config.sh
@@ -8,8 +8,8 @@ lb config \
   --apt-source-archives false \
   -a amd64 \
   -b iso-hybrid \
-  --bootappend-live "boot=live components loglevel=3 net.ifnames=0" \
-  --bootappend-live-failsafe "boot=live components memtest noapic noapm nodma nomce nolapic nomodeset nosmp nosplash vga=normal net.ifnames=0" \
+  --bootappend-live "boot=live components loglevel=3" \
+  --bootappend-live-failsafe "boot=live components memtest noapic noapm nodma nomce nolapic nomodeset nosmp nosplash vga=normal" \
   --bootloaders "syslinux" \
   --zsync false \
   --debootstrap-options "--variant=minbase" \
