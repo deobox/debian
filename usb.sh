6b2fe53156424615411aa388e33e086038866dc5
Dimitar Dimitrov <deobox@gmail.com>
2022-08-17 14:23:04 +0100

    USB update

--- usb.sh
+++ usb.sh
@@ -12,7 +12,7 @@ fi
 
 DEVICE=$1
 
-apt-get install syslinux-common syslinux gdisk parted
+apt-get install syslinux-common syslinux gdisk parted systemd-boot
 
 mkdir -p build/live
 wget http://boot.test.net.in/live/bullseye/filesystem.squashfs -O build/live/filesystem.squashfs
